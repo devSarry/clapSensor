8c538f31d26b9ed2c3c53d26354baa7fc861ed98
Adam West <jsarry@gmail.com>
2017-10-30 14:54:49 +0200

    Add blink func and counter

--- clap/clap.ino
+++ clap/clap.ino
@@ -1,15 +1,17 @@
+
+
 int Green_Led = 10;
-int Red_Led = 11;
+
 int Mic_Pin = 12;
 
 int micVal = HIGH;
+int clapCounter = 0;
 
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(Green_Led, OUTPUT);
-  pinMode(Red_Led, OUTPUT);
 
   pinMode(Mic_Pin, INPUT);
 
@@ -23,16 +25,47 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
   micVal = digitalRead(Mic_Pin);
   
-
   if (micVal == HIGH) {
-    Serial.println("Heard Something!!!");
-    digitalWrite(Green_Led, HIGH);
-    delay(500);
+
+    double startTime = millis();
+
+    while( millis() - startTime <= 3000) {
+        micVal = digitalRead(Mic_Pin);
+        digitalWrite(Green_Led, HIGH);
+
+        if (micVal == HIGH) {
+          clapCounter++;
+          delay(100);
+        }
+
+        
+    }
     digitalWrite(Green_Led, LOW);
+
+    int clapSQR = clapCounter * clapCounter;
+
+    Serial.print("The number of claps^2: ");
+    Serial.println(clapSQR );
+    Serial.print("In Binary: ");
+    Serial.println(clapSQR, BIN);
+    Serial.print("In HEX: ");
+    Serial.println(clapSQR, HEX);
+    Serial.print("In OCT: ");
+    Serial.println(clapSQR, OCT);
+    
+
+    for ( int init = 0; init <= clapSQR; init++) {
+      digitalWrite(Green_Led, HIGH);
+      delay(100);
+      digitalWrite(Green_Led, LOW);
+      delay(100); 
+      
+    }
+
+    
 
   }
 
